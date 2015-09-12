63263c548e8f19336e073d3e2491b72579efe959
saasbook <saasbook@saasbook.(none)>
2015-09-12 12:41:41 -0300

    to do

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,12 +1,23 @@
 class Dessert
   def initialize(name, calories)
     # your code here
+    @name = name
+    @calories = calories
   end
   def healthy?
     # your code here
+    if @calories < 200 && delicious?
+      return true
+    end
   end
   def delicious?
     # your code here
+  end
+  def name
+    @name
+  end
+  def calories
+    @calories
   end
 end
 
