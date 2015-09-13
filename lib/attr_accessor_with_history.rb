6894cbe1012c89d01706201d360fb8f852084977
saasbook <saasbook@saasbook.(none)>
2015-09-13 04:03:11 -0300

    Part 4 accomplished

--- lib/attr_accessor_with_history.rb
+++ lib/attr_accessor_with_history.rb
@@ -5,6 +5,27 @@ class Class
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
       # YOUR CODE HERE
+      def foo
+      	@foo
+      end
+
+      def foo=(n)
+      	if @foo_history == nil
+      		@foo_history = [nil]
+      		@foo=n
+      	else
+      		@foo_history << @foo
+      		@foo=n
+      	end
+      	
+      	@foo
+      end
+
+      def foo_history
+      	@foo_history
+      end
+
+      
     }
   end
 end
