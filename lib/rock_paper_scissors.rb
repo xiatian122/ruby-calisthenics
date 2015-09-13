fdcf0eb85da77f80269da0c41ff2cb2278bec942
saasbook <saasbook@saasbook.(none)>
2015-09-13 02:43:39 -0300

    Part 3 Accomplished

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -4,11 +4,49 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    # Strategy Check
+    case player1[1]
+    when "P"
+    when "R"
+    when "S"
+    else # raise "Strategy must be one of R,P,S"
+        raise NoSuchStrategyError, "Strategy must be one of R,P,S"
+    end
+
+    case player2[1]
+    when "P"
+    when "R"
+    when "S"
+    else # raise "Strategy must be one of R,P,S"
+        raise NoSuchStrategyError, "Strategy must be one of R,P,S"
+    end
+
+    # The same strategy
+    if player1[1] == player2[1]
+    	return player1
+    end
+    # Then perform comparison
+    case player1[1]+player2[1]
+    when "PR"
+    	return player1
+    when "RS"
+    	return player1
+    when "SP"
+    	return player1
+    else
+    	return player2
+    end
   end
 
   def self.tournament_winner(tournament)
     # YOUR CODE HERE
+    if tournament[1].class.to_s == "String"
+        return tournament
+    else
+        return self.winner(self.tournament_winner(tournament[0]), self.tournament_winner(tournament[1]))
+    end
+
+
   end
 
 end
